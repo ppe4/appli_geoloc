32f4c40ec3e8e97d573db403b8250641da50c5bd
gelineaua <gelineaua@saintemarie-cholet.eu>
2015-02-26 10:57:57 +0100

    On refait ça propre

---
+++ src/appli_geoloc/Main.java
@@ -0,0 +1,10 @@
+package appli_geoloc;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("is this swag ?");
+	}
+
+}
--- src/appli_geoloc/bonjour.java
+++
@@ -1,5 +0,0 @@
-package appli_geoloc;
-
-public class bonjour {
-	System.out.println('olol');
-}
